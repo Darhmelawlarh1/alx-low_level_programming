feb62568d1397dbe7b549c448d2b1a1b241394ee
Darhmelawlarh1 <opadiranafeez4real@gmail.com>
2022-12-08 15:21:05 -0800

    My first commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,10 +6,10 @@
 */
 int main(void)
 {
-printf("Size of a char: %lu byte(S)\n", sizeof(char));
-printf("Size of an int: %lu byte(S)\n", sizeof(int));
-printf("Size of a long  int: %lu byte(S)\n", sizeof(long init));
-printf("Size of a long long  int: %lu byte(S)\n", sizeof(long long init));
-printf("Size of a float: %lu byte(S)\n", sizeof(float));
+printf("Size of a char: %i byte(S)\n", sizeof(char));
+printf("Size of an int: %i byte(S)\n", sizeof(int));
+printf("Size of a long  int: %i byte(S)\n", sizeof(long init));
+printf("Size of a long long  int: %i byte(S)\n", sizeof(long long init));
+printf("Size of a float: %i byte(S)\n", sizeof(float));
 return (0);
 }
