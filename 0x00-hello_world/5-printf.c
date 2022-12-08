365d85b24e89ef80baad6f80c88bb5bc46c10b93
Darhmelawlarh1 <opadiranafeez4real@gmail.com>
2022-12-08 13:46:30 -0800

    My first commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+* main - Entry point print a line of code using printf
+* Return: 0
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
