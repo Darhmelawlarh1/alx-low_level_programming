f457289ffaa2fdf56ae2d90463d802aea184a4e2
Darhmelawlarh1 <opadiranafeez4real@gmail.com>
2023-01-02 07:49:39 -0800

    Mozart composed his

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,26 +1,31 @@
 #include "main.h"
 
 /**
- * leet - function that encode a string
- * @x: the string
- * Return: the string encoded
+ * leet - Encodes a string into 1337
+ * @c: String of variable pointer
+ * Return: String that is encoded
  */
-char *leet(char *x)
-{
-	int a = 0, b, l = 5;
-	char tr[5] = {'A', 'E', '0', 'T', 'L'};
-	char trw[5] = {'4', '3', '0', '7', '1'};
 
-	while (x[a])
+char *leet(char *c)
+{
+	char *cp = c;
+	char key[] = {'A', 'E', 'O', 'T', 'L'};
+	int value[] = {4, 3, 0, 7, 1};
+	unsigned int i;
+
+	while (*c)
 	{
-		b = 0;
-		while (b < l)
+		for (i = 0; i < sizeof(key) / sizeof(char); i++)
 		{
-			if (x[a] == tr[b] || x[a] - 32 == tr[b])
-				x[a] = trw[b];
-			b++;
+			/*32 is the difference between lower case letters and apper case letters*/
+			if (*c == key[i] || *c == key[i] + 32)
+			{
+				*c = 48 + value[i];
+			}
 		}
-		a++;
+		c++;
 	}
-	return (x);
+
+	return (cp);
+
 }
